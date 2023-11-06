010001dbc1074ce58472d0e708c3b19f11f7d6cc
Anja Petković <anjapetkovic92@gmail.com>
2023-11-06 22:53:54 +0100

    Door buzzer
    Implementation of buzzer and its simulation

--- Project/main.py
+++ Project/main.py
@@ -1,3 +1,4 @@
+from components.buzzer.buzzer import Buzzer
 from components.door_light.dioda import Dioda
 from components.door_sensor.door_sensor import MotionSensor
 from project_settings.settings import load_settings
@@ -8,14 +9,21 @@ def run_door_light(settings):
     door_light = Dioda(door_light_settings['pin'], door_light_settings['simulated'])
     door_light.run([])
 
+
 def run_door_motion(settings):
     motion_sensor_settings = settings['DS1']
     motion_sensor = MotionSensor(motion_sensor_settings['pin'], motion_sensor_settings['simulated'])
     motion_sensor.run([])
 
 
+def run_buzzer(settings):
+    buzzer_settings = settings['DS1']
+    door_buzzer = Buzzer(buzzer_settings['pin'], buzzer_settings['simulated'])
+    door_buzzer.run([])
+
+
 if __name__ == '__main__':
     settings = load_settings()
     # run_door_light(settings)
-    run_door_motion(settings)
-
+    # run_door_motion(settings)
+    run_buzzer(settings)
