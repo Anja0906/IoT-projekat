0d91476d0845721781f387973160677dbdcdac79
branislavstojkovic70 <96660978+branislavstojkovic70@users.noreply.github.com>
2023-11-06 21:00:42 +0100

    Door motion
    Door motion added
    Simulation and real use case
    Using pir for motion detection

---
+++ Project/components/door_sensor/door_sensor.py
@@ -0,0 +1,51 @@
+
+import time
+import random
+import threading
+
+
+class MotionSensor(object):
+    def __init__(self, pin, simulated):
+        self.pin = pin
+        self.simulated = simulated
+
+    def motion_detected(self, channel):
+        print("You moved")
+
+    def no_motion(self, channel):
+        print("You stopped moving")
+
+    def run_motion(self):
+        from RPi.GPIO import GPIO
+        GPIO.setmode(GPIO.BCM)
+        GPIO.setup(self.pin, GPIO.IN)
+        GPIO.add_event_detect(self.pin, GPIO.RISING, callback=self.motion_detected)
+        input("Press any key to exit...")
+
+    def run_simulation(self):
+        while True:
+            rnd = random.randint(0, 1)
+            if rnd == 1:
+                self.motion_detected(None)
+            else:
+                self.no_motion(None)
+            time.sleep(1)
+
+    def run(self, threads):
+        stop_event = threading.Event()
+        try:
+            if self.simulated:
+                print("Starting motion simulator")
+                motion_thread = threading.Thread(target=self.run_simulation, args=())
+                motion_thread.start()
+                threads.append(motion_thread)
+            elif not self.simulated:
+                print("Starting motion loop")
+                motion_thread = threading.Thread(target=self.run_motion, args=())
+                motion_thread.start()
+                threads.append(motion_thread)
+            else:
+                print("Self.simulated is none")
+        except KeyboardInterrupt:
+            for t in threads:
+                stop_event.set()
--- Project/main.py
+++ Project/main.py
@@ -1,4 +1,5 @@
-from door_light.dioda import Dioda
+from components.door_light.dioda import Dioda
+from components.door_sensor.door_sensor import MotionSensor
 from project_settings.settings import load_settings
 
 
@@ -7,7 +8,14 @@ def run_door_light(settings):
     door_light = Dioda(door_light_settings['pin'], door_light_settings['simulated'])
     door_light.run([])
 
+def run_door_motion(settings):
+    motion_sensor_settings = settings['DS1']
+    motion_sensor = MotionSensor(motion_sensor_settings['pin'], motion_sensor_settings['simulated'])
+    motion_sensor.run([])
+
 
 if __name__ == '__main__':
     settings = load_settings()
-    run_door_light(settings)
+    # run_door_light(settings)
+    run_door_motion(settings)
+
