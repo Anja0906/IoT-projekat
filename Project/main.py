9184d90a507b7f6ca4c31bebc84de04d50a42ed7
Anja Petković <anjapetkovic92@gmail.com>
2023-11-06 20:29:52 +0100

    Refactoring code for door light

--- Project/main.py
+++ Project/main.py
@@ -1,1 +1,13 @@
-print("py1")
+from door_light.dioda import Dioda
+from project_settings.settings import load_settings
+
+
+def run_door_light(settings):
+    door_light_settings = settings['DL']
+    door_light = Dioda(door_light_settings['pin'], door_light_settings['simulated'])
+    door_light.run([])
+
+
+if __name__ == '__main__':
+    settings = load_settings()
+    run_door_light(settings)
