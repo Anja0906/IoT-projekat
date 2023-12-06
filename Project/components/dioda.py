4ec4cb4bdf93b9383f38d55bc327db80d694d1ad
Anja Petković <anjapetkovic92@gmail.com>
2023-12-06 23:40:36 +0100

    Door light updated
    Updated code for door light as dioda to write value true or false if it is on or off

--- Project/components/dioda.py
+++ Project/components/dioda.py
@@ -1,11 +1,36 @@
 import threading
 import time
+import json
+from datetime import datetime
+
+import paho.mqtt.publish as publish
+from sensors.broker_settings import HOSTNAME, PORT
 
 result = True
+dioda_batch = []
+publish_data_counter = 0
+publish_data_limit = 5
+counter_lock = threading.Lock()
+
+def publisher_task(event, batch):
+    global publish_data_counter, publish_data_limit
+    while True:
+        event.wait()
+        with counter_lock:
+            local_dioda_batch = batch.copy()
+            publish_data_counter = 0
+            batch.clear()
+        publish.multiple(local_dioda_batch, hostname=HOSTNAME, port=PORT)
+        event.clear()
 
 
-def dl_callback():
-    global result
+publish_event = threading.Event()
+publisher_thread = threading.Thread(target=publisher_task, args=(publish_event, dioda_batch))
+publisher_thread.daemon = True
+publisher_thread.start()
+
+def dl_callback(publish_event, settings, code, verbose=False):
+    global result, publish_data_counter, publish_data_limit
     t = time.localtime()
     print("=" * 20)
     print(f"Timestamp: {time.strftime('%H:%M:%S', t)}")
@@ -15,11 +40,25 @@ def dl_callback():
     else:
         result = True
         print("Light is off\n")
+    dioda_payload = {
+        "measurement": "DoorLight",
+        "simulated": settings['simulated'],
+        "runs_on": settings["runs_on"],
+        "name": settings["name"],
+        "value": result
+    }
+
+    with counter_lock:
+        dioda_batch.append(('DoorLight', json.dumps(dioda_payload), 0, True))
+        publish_data_counter += 1
+
+    if publish_data_counter >= publish_data_limit:
+        publish_event.set()
 
 
 def run_dl(settings, threads, stop_event, code):
     if settings['simulated']:
-        dl_thread = threading.Thread(target=dl_callback, args=())
+        dl_thread = threading.Thread(target=dl_callback, args=(publish_event, settings, code))
         dl_thread.start()
         threads.append(dl_thread)
     else:
