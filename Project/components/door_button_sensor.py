4fbe05d05cf87b8202383f66fc3dca46eb70216f
Anja Petković <anjapetkovic92@gmail.com>
2023-12-30 19:47:09 +0100

    Alarm added
    Alarm added

--- Project/components/door_button_sensor.py
+++ Project/components/door_button_sensor.py
@@ -28,19 +28,8 @@ publisher_thread.daemon = True
 publisher_thread.start()
 
 
-def ds_callback(motion, publish_event, settings, code, verbose=False):
+def ds_callback(motion, publish_event, settings, code):
     global door_open, publish_data_counter, publish_data_limit
-    if verbose:
-        t = time.localtime()
-        print("=" * 20)
-        print(f"Timestamp: {time.strftime('%H:%M:%S', t)}")
-        print(f"Code: {code}")
-        if door_open:
-            door_open = not door_open
-            print("Door is currently opened")
-        else:
-            door_open = not door_open
-            print("Door is currently closed")
     door_sensor_payload = {
         "measurement": "DoorSensor",
         "simulated": settings['simulated'],
@@ -61,7 +50,7 @@ def run_ds(settings, threads, stop_event, code):
     if settings['simulated']:
         from sensors.s_simulators.door_sensor import run_ds_simulator
         print("Starting " + code + " simulator")
-        ds_thread = threading.Thread(target=run_ds_simulator, args=(2, ds_callback, stop_event, publish_event, settings, code))
+        ds_thread = threading.Thread(target=run_ds_simulator, args=(10, ds_callback, stop_event, publish_event, settings, code))
         ds_thread.start()
         threads.append(ds_thread)
         print(code + " simulator started\n")
